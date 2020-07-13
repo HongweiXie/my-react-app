f11790cf6fd56471a4324dddc665e0b269964103
hongwei.xhw <hongwei.xhw@alibaba-inc.com>
2020-07-13 20:20:19 +0800

    Updates

---
+++ precache-manifest.cef974c07b3431e2013201371932e64b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2703073c4402812cb5083e49afdd6136",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "f5e44665873f84ceaf58",
+    "url": "/my-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd00b1a5dae03b8e725c",
+    "url": "/my-react-app/static/js/2.0c8c0601.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/my-react-app/static/js/2.0c8c0601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e44665873f84ceaf58",
+    "url": "/my-react-app/static/js/main.d003cf7d.chunk.js"
+  },
+  {
+    "revision": "f43a4e717c8594e2b093",
+    "url": "/my-react-app/static/js/runtime-main.034b7318.js"
+  }
+]);
